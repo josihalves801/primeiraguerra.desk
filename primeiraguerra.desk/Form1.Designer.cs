a32f1b2316a0090e048c990b4a34ff7eae4356cf
Josih Alves <josilenealves801@gmail.com>
2018-04-05 10:03:23 -0300

    comecou

--- primeiraguerra.desk/Form1.Designer.cs
+++ primeiraguerra.desk/Form1.Designer.cs
@@ -28,7 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fase1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fase2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fase3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fase1ToolStripMenuItem,
+            this.fase2ToolStripMenuItem,
+            this.fase3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1664, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fase1ToolStripMenuItem
+            // 
+            this.fase1ToolStripMenuItem.Name = "fase1ToolStripMenuItem";
+            this.fase1ToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fase1ToolStripMenuItem.Text = "Fase 1";
+            this.fase1ToolStripMenuItem.Click += new System.EventHandler(this.fase1ToolStripMenuItem_Click);
+            // 
+            // fase2ToolStripMenuItem
+            // 
+            this.fase2ToolStripMenuItem.Name = "fase2ToolStripMenuItem";
+            this.fase2ToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fase2ToolStripMenuItem.Text = "Fase 2";
+            this.fase2ToolStripMenuItem.Click += new System.EventHandler(this.fase2ToolStripMenuItem_Click);
+            // 
+            // fase3ToolStripMenuItem
+            // 
+            this.fase3ToolStripMenuItem.Name = "fase3ToolStripMenuItem";
+            this.fase3ToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fase3ToolStripMenuItem.Text = "Fase 3";
+            this.fase3ToolStripMenuItem.Click += new System.EventHandler(this.fase3ToolStripMenuItem_Click);
             // 
             // Menu
             // 
@@ -36,13 +74,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.qui;
             this.ClientSize = new System.Drawing.Size(1664, 1012);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fase1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fase2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fase3ToolStripMenuItem;
     }
 }
 
--- primeiraguerra.desk/Form1.cs
+++ primeiraguerra.desk/Form1.cs
@@ -16,5 +16,29 @@ namespace primeiraguerra.desk
         {
             InitializeComponent();
         }
+
+        private void fase1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 tela_fase1 = new Form2();
+            this.Visible = false;
+            tela_fase1.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void fase2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 tela_fase2 = new Form3();
+            this.Visible = false;
+            tela_fase2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void fase3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 tela_fase3 = new Form4();
+            this.Visible = false;
+            tela_fase3.ShowDialog();
+            this.Visible = true;
+        }
     }
 }
