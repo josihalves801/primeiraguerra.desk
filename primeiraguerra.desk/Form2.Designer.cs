b8ebf4231bb444dc5289c4eef7545590e232c8e3
Josih Alves <josilenealves801@gmail.com>
2018-04-12 09:51:11 -0300

    conflito resolvido

--- primeiraguerra.desk/Form2.Designer.cs
+++ primeiraguerra.desk/Form2.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.btntentaragain = new System.Windows.Forms.Button();
             this.btnveriQ01 = new System.Windows.Forms.Button();
             this.Q01Opcao4 = new System.Windows.Forms.RadioButton();
@@ -39,7 +40,17 @@
             this.q01Opcao1 = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -58,6 +69,8 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.btntentaragain);
             this.panel1.Controls.Add(this.btnveriQ01);
             this.panel1.Controls.Add(this.Q01Opcao4);
@@ -70,14 +83,25 @@
             this.panel1.Size = new System.Drawing.Size(587, 365);
             this.panel1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.proximo_botao_318_42554;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(402, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 61);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // btntentaragain
             // 
             this.btntentaragain.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.symbol_909831_960_720;
             this.btntentaragain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btntentaragain.Enabled = false;
-            this.btntentaragain.Location = new System.Drawing.Point(384, 103);
+            this.btntentaragain.Location = new System.Drawing.Point(402, 116);
             this.btntentaragain.Name = "btntentaragain";
-            this.btntentaragain.Size = new System.Drawing.Size(97, 92);
+            this.btntentaragain.Size = new System.Drawing.Size(79, 64);
             this.btntentaragain.TabIndex = 6;
             this.btntentaragain.UseVisualStyleBackColor = true;
             this.btntentaragain.Click += new System.EventHandler(this.btntentaragain_Click);
@@ -86,9 +110,9 @@
             // 
             this.btnveriQ01.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.images;
             this.btnveriQ01.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnveriQ01.Location = new System.Drawing.Point(384, 201);
+            this.btnveriQ01.Location = new System.Drawing.Point(402, 186);
             this.btnveriQ01.Name = "btnveriQ01";
-            this.btnveriQ01.Size = new System.Drawing.Size(97, 95);
+            this.btnveriQ01.Size = new System.Drawing.Size(79, 61);
             this.btnveriQ01.TabIndex = 5;
             this.btnveriQ01.UseVisualStyleBackColor = true;
             this.btnveriQ01.Click += new System.EventHandler(this.btnveriQ01_Click);
@@ -148,6 +172,109 @@
             this.label2.Text = "1) Em que ano ocorreu a Primeira Guerra Mundial?";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.radioButton3);
+            this.panel2.Controls.Add(this.radioButton4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(349, 346);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(587, 365);
+            this.panel2.TabIndex = 8;
+            this.panel2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.proximo_botao_318_42554;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(402, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 61);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.symbol_909831_960_720;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(402, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 64);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.images;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(402, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 61);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(77, 252);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(82, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1918 - 1920";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(77, 201);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "1939 - 1945";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(77, 154);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(82, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1914 - 1918";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(77, 106);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(82, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1920 - 1924";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label3.Location = new System.Drawing.Point(74, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(458, 28);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "1) Em que ano ocorreu a Primeira Guerra Mundial?";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -164,6 +291,8 @@
             this.Load += new System.EventHandler(this.Fase2_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,5 +310,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btntentaragain;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label3;
     }
 }
