589d31f6bd0d173c7917e9b16e143a7da8c1ffb2
Yasmin Licarião Maia Gil Gusmon <yaya.gusmon@gmail.com>
2018-04-23 09:20:12 -0300

    ww

--- primeiraguerra.desk/Form3.Designer.cs
+++ primeiraguerra.desk/Form3.Designer.cs
@@ -31,12 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.proximaq = new System.Windows.Forms.Button();
+            this.vefiq1 = new System.Windows.Forms.Button();
+            this.btnnovatenta1 = new System.Windows.Forms.Button();
+            this.q1r5 = new System.Windows.Forms.RadioButton();
+            this.q1r4 = new System.Windows.Forms.RadioButton();
+            this.q1r3 = new System.Windows.Forms.RadioButton();
+            this.q1r2 = new System.Windows.Forms.RadioButton();
+            this.q1r1 = new System.Windows.Forms.RadioButton();
+            this.q1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,98 +48,135 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(146, 52);
+            this.label1.Location = new System.Drawing.Point(134, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1006, 34);
+            this.label1.Size = new System.Drawing.Size(966, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Neste nível, trataremos sobre alguns dos países que estavam envolvidos";
+            this.label1.Text = "Neste nível, abordaremos sobre diversos temas dos países envolvidos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.radioButton5);
-            this.panel1.Controls.Add(this.radioButton4);
-            this.panel1.Controls.Add(this.radioButton3);
-            this.panel1.Controls.Add(this.radioButton2);
-            this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(258, 174);
+            this.panel1.Controls.Add(this.proximaq);
+            this.panel1.Controls.Add(this.vefiq1);
+            this.panel1.Controls.Add(this.btnnovatenta1);
+            this.panel1.Controls.Add(this.q1r5);
+            this.panel1.Controls.Add(this.q1r4);
+            this.panel1.Controls.Add(this.q1r3);
+            this.panel1.Controls.Add(this.q1r2);
+            this.panel1.Controls.Add(this.q1r1);
+            this.panel1.Controls.Add(this.q1);
+            this.panel1.Location = new System.Drawing.Point(245, 171);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(714, 439);
             this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // label2
+            // proximaq
             // 
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.label2.Location = new System.Drawing.Point(8, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(704, 117);
-            this.label2.TabIndex = 0;
-            this.label2.Text = resources.GetString("label2.Text");
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.proximaq.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.proximo_botao_318_42554;
+            this.proximaq.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.proximaq.Location = new System.Drawing.Point(452, 345);
+            this.proximaq.Name = "proximaq";
+            this.proximaq.Size = new System.Drawing.Size(86, 66);
+            this.proximaq.TabIndex = 8;
+            this.proximaq.UseVisualStyleBackColor = true;
+            this.proximaq.Click += new System.EventHandler(this.proximaq_Click);
             // 
-            // radioButton1
+            // vefiq1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 18F);
-            this.radioButton1.Location = new System.Drawing.Point(187, 133);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(274, 31);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = " Ir aos campos de batalha";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.vefiq1.BackgroundImage = global::primeiraguerra.desk.Properties.Resources.verificar_contorno_preto_marca_318_49519;
+            this.vefiq1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.vefiq1.Location = new System.Drawing.Point(315, 345);
+            this.vefiq1.Name = "vefiq1";
+            this.vefiq1.Size = new System.Drawing.Size(86, 66);
+            this.vefiq1.TabIndex = 7;
+            this.vefiq1.UseVisualStyleBackColor = true;
+            this.vefiq1.Click += new System.EventHandler(this.vefiq1_Click);
             // 
-            // radioButton2
+            // btnnovatenta1
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 18F);
-            this.radioButton2.Location = new System.Drawing.Point(187, 170);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(239, 31);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Trabalhar em fábricas";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.btnnovatenta1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnnovatenta1.BackgroundImage")));
+            this.btnnovatenta1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnnovatenta1.Location = new System.Drawing.Point(173, 345);
+            this.btnnovatenta1.Name = "btnnovatenta1";
+            this.btnnovatenta1.Size = new System.Drawing.Size(86, 66);
+            this.btnnovatenta1.TabIndex = 6;
+            this.btnnovatenta1.UseVisualStyleBackColor = true;
+            this.btnnovatenta1.Click += new System.EventHandler(this.btnnovatenta1_Click_1);
             // 
-            // radioButton3
+            // q1r5
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Times New Roman", 18F);
-            this.radioButton3.Location = new System.Drawing.Point(187, 207);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(157, 31);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Dirigir carros";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.q1r5.AutoSize = true;
+            this.q1r5.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.q1r5.Location = new System.Drawing.Point(187, 281);
+            this.q1r5.Name = "q1r5";
+            this.q1r5.Size = new System.Drawing.Size(81, 31);
+            this.q1r5.TabIndex = 5;
+            this.q1r5.TabStop = true;
+            this.q1r5.Text = "Votar";
+            this.q1r5.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // q1r4
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Font = new System.Drawing.Font("Times New Roman", 18F);
-            this.radioButton4.Location = new System.Drawing.Point(187, 244);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(263, 31);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Entrar em universidades";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.q1r4.AutoSize = true;
+            this.q1r4.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.q1r4.Location = new System.Drawing.Point(187, 244);
+            this.q1r4.Name = "q1r4";
+            this.q1r4.Size = new System.Drawing.Size(263, 31);
+            this.q1r4.TabIndex = 4;
+            this.q1r4.TabStop = true;
+            this.q1r4.Text = "Entrar em universidades";
+            this.q1r4.UseVisualStyleBackColor = true;
             // 
-            // radioButton5
+            // q1r3
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Font = new System.Drawing.Font("Times New Roman", 18F);
-            this.radioButton5.Location = new System.Drawing.Point(187, 281);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(81, 31);
-            this.radioButton5.TabIndex = 5;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Votar";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.q1r3.AutoSize = true;
+            this.q1r3.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.q1r3.Location = new System.Drawing.Point(187, 207);
+            this.q1r3.Name = "q1r3";
+            this.q1r3.Size = new System.Drawing.Size(157, 31);
+            this.q1r3.TabIndex = 3;
+            this.q1r3.TabStop = true;
+            this.q1r3.Text = "Dirigir carros";
+            this.q1r3.UseVisualStyleBackColor = true;
+            // 
+            // q1r2
+            // 
+            this.q1r2.AutoSize = true;
+            this.q1r2.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.q1r2.Location = new System.Drawing.Point(187, 170);
+            this.q1r2.Name = "q1r2";
+            this.q1r2.Size = new System.Drawing.Size(239, 31);
+            this.q1r2.TabIndex = 2;
+            this.q1r2.TabStop = true;
+            this.q1r2.Text = "Trabalhar em fábricas";
+            this.q1r2.UseVisualStyleBackColor = true;
+            // 
+            // q1r1
+            // 
+            this.q1r1.AutoSize = true;
+            this.q1r1.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.q1r1.Location = new System.Drawing.Point(187, 133);
+            this.q1r1.Name = "q1r1";
+            this.q1r1.Size = new System.Drawing.Size(274, 31);
+            this.q1r1.TabIndex = 1;
+            this.q1r1.TabStop = true;
+            this.q1r1.Text = " Ir aos campos de batalha";
+            this.q1r1.UseVisualStyleBackColor = true;
+            // 
+            // q1
+            // 
+            this.q1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.q1.Location = new System.Drawing.Point(8, 13);
+            this.q1.Name = "q1";
+            this.q1.Size = new System.Drawing.Size(704, 117);
+            this.q1.TabIndex = 0;
+            this.q1.Text = resources.GetString("q1.Text");
+            this.q1.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form3
             // 
@@ -162,11 +202,14 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label q1;
+        private System.Windows.Forms.RadioButton q1r5;
+        private System.Windows.Forms.RadioButton q1r4;
+        private System.Windows.Forms.RadioButton q1r3;
+        private System.Windows.Forms.RadioButton q1r2;
+        private System.Windows.Forms.RadioButton q1r1;
+        private System.Windows.Forms.Button btnnovatenta1;
+        private System.Windows.Forms.Button proximaq;
+        private System.Windows.Forms.Button vefiq1;
     }
 }
